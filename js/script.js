8651cec2af3d1ab83f3c7887952f54923de6081d
Ethan Strauss <estrauss@gmail.com>
2018-08-10 19:59:18 -0400

    A whole day on something tiny...

--- js/script.js
+++ js/script.js
@@ -1,34 +1,53 @@
 'use strict';
+window.onload = function event() {
 
-window.addEventListener('scroll', scrolling);
-
-function scrolling(e) {
-	const windowTop = window.pageYOffset;
-	const topbar = document.querySelector('.topbar');
-	const logo = document.querySelector('.logo');
-	const nav = document.querySelector('.nav');
-	// const fact = document.querySelector('#fact-slice');
-	const portfolio = document.querySelector('#portfolio-slice');
-	const stars = document.querySelectorAll('.fa-star');
-   	const distanceToTop = portfolio.getBoundingClientRect().top;
-    // const y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
-    // const screenBottom = windowTop+y;
-    console.log(distanceToTop);
-
-	if(windowTop > 40 && !topbar.classList.contains('scroll')) {
-		logo.classList.add('logo--scroll') ;
-		topbar.classList.add('topbar--scroll');
-		nav.classList.add('nav--scroll');
-	}
-	if(windowTop < 40) {
-		if(logo.classList.contains('logo--scroll')) logo.classList.remove('logo--scroll');
-		if(topbar.classList.contains('topbar--scroll')) topbar.classList.remove('topbar--scroll');
-		if(nav.classList.contains('nav--scroll')) nav.classList.remove('nav--scroll');
-	}
-	console.log
-	if (distanceToTop <= -50) {
-		stars.forEach(star => { 
-			if(star.classList.contains('reveal')) star.classList.remove('reveal'); 
+	// Make the stars first scale in then swap to rotate anim.
+	function swap() {
+		const starArry = document.querySelectorAll('.fa-star');
+		starArry.forEach(star => { 
+			star.classList.remove('reveal');
+			star.classList.add('spin'); 
 		});
 	}
+
+	// Client scrolls, everythign comes in.
+	window.addEventListener('scroll', scrolling, false);
+
+	function scrolling(e) {
+
+		const windowTop = window.pageYOffset;
+		const topbar = document.querySelector('.topbar');
+		const logo = document.querySelector('.logo');
+		const nav = document.querySelector('.nav');
+		const portfolio = document.querySelector('#portfolio-slice');
+	   	const PortfolioTop = portfolio.getBoundingClientRect().top;
+		const stars = document.querySelectorAll('.fa-star');
+		const bell = document.querySelector('.bell');
+
+		if(windowTop > 40 && !topbar.classList.contains('scroll')) {
+			logo.classList.add('logo--scroll') ;
+			topbar.classList.add('topbar--scroll');
+			nav.classList.add('nav--scroll');
+		}
+		if(windowTop < 40) {
+			if(logo.classList.contains('logo--scroll')) logo.classList.remove('logo--scroll');
+			if(topbar.classList.contains('topbar--scroll')) topbar.classList.remove('topbar--scroll');
+			if(nav.classList.contains('nav--scroll')) nav.classList.remove('nav--scroll');
+		}
+		
+		if (PortfolioTop <= -30) {
+
+			if(bell.classList.contains('bellin')) bell.classList.remove('bellin');
+
+			if (!stars[0].classList.contains('in')) {
+				stars.forEach(star => { 
+					star.classList.add('reveal');
+					star.classList.add('in'); 
+				});
+			}
+
+			if(stars[0].classList.contains('reveal')) setTimeout(function(){ swap(); }, 2000); 
+		}
+	}
+
 }
