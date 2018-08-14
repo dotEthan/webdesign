9b0322ade2473a6deac0c4d54ea3ed872c36c100
Ethan Strauss <estrauss@gmail.com>
2018-08-13 20:19:36 -0400

    added testimonials, finished animations, organized hero page and added topbar functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 'use strict';
 window.onload = function event() {
 
+
 	// Make the stars first scale in then swap to rotate anim.
+	function cartSwap() {
+
+		document.querySelector('.cart').classList.add('cart--hover');
+		document.querySelector('.fact__gift').classList.add('gift--hover');
+		document.querySelector('.facebook').classList.add('facebook--hover');
+		document.querySelector('.twitter').classList.add('twitter--hover');
+		document.querySelector('.linkedin').classList.add('linkedin--hover');
+		document.querySelector('.dribbble').classList.add('dribbble--hover');
+	}
+
 	function swap() {
 		const dots = document.querySelectorAll('.dot');
 		const starArry = document.querySelectorAll('.fa-star');
@@ -10,14 +21,14 @@ window.onload = function event() {
 			star.classList.add('spin'); 
 		});
 
-		if (!dots[0].classList.contains('dot--gone')) {
-			console.log('now');
-			dots.forEach(dot => { 
+		if(!dots[0].classList.contains('dot--gone')) {
+			dots.forEach(dot => {
 				dot.classList.add('dot--gone');
-
 			});
 		}
 	}
+	let lastScroll = 0;
+	let direction = true;
 
 	// Client scrolls, everythign comes in.
 	window.addEventListener('scroll', scrolling, false);
@@ -31,17 +42,18 @@ window.onload = function event() {
 		const portfolio = document.querySelector('#portfolio-slice');
 	   	const PortfolioTop = portfolio.getBoundingClientRect().top;
 		const stars = document.querySelectorAll('.fa-star');
-		const bell = document.querySelector('.bell');
 		const tower = document.querySelector('.tower');
 		const signals = document.querySelectorAll('.signal');
+		const socials = document.querySelectorAll('.social--media');
 		const cart = document.querySelector('.cart');
-
+		const gift = document.querySelector('.fact__gift');
+		
 		if(windowTop > 40 && !topbar.classList.contains('scroll')) {
 			logo.classList.add('logo--scroll') ;
 			topbar.classList.add('topbar--scroll');
 			nav.classList.add('nav--scroll');
 		}
-		if(windowTop < 40) {
+		if(windowTop < 40 || windowTop < lastScroll) {
 			if(logo.classList.contains('logo--scroll')) logo.classList.remove('logo--scroll');
 			if(topbar.classList.contains('topbar--scroll')) topbar.classList.remove('topbar--scroll');
 			if(nav.classList.contains('nav--scroll')) nav.classList.remove('nav--scroll');
@@ -49,9 +61,11 @@ window.onload = function event() {
 		
 		if (PortfolioTop <= -30) {
 
-			if(bell.classList.contains('none')) bell.classList.remove('none');
+			if(cart.classList.contains('none')) setTimeout(function(){ cartSwap(); }, 1000); 
 
 			if(cart.classList.contains('none')) cart.classList.remove('none');
+
+			if(gift.classList.contains('none')) gift.classList.remove('none');
 
 			if(tower.classList.contains('none')) tower.classList.remove('none');
 
@@ -68,7 +82,15 @@ window.onload = function event() {
 				});
 			}
 
+			if (socials[0].classList.contains('none')) {
+				socials.forEach(social => { 
+					social.classList.remove('none');
+				});
+			}
+
 			if(stars[0].classList.contains('reveal')) setTimeout(function(){ swap(); }, 2000); 
 		}
+
+		lastScroll = windowTop;
 	}
 }
