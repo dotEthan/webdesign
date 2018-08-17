158513bd9936234651af8e9295471e2ab0a79cb6
Ethan Strauss <estrauss@gmail.com>
2018-08-17 19:37:21 -0400

    responsive animations

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 'use strict';
 window.onload = function event() {
-
-
 	// Make the stars first scale in then swap to rotate anim.
 	function cartSwap() {
-
 		document.querySelector('.cart').classList.add('cart--hover');
 		document.querySelector('.fact__gift').classList.add('gift--hover');
 		document.querySelector('.facebook').classList.add('facebook--hover');
@@ -16,17 +13,18 @@ window.onload = function event() {
 	function swap() {
 		const dots = document.querySelectorAll('.dot');
 		const starArry = document.querySelectorAll('.fa-star');
-		starArry.forEach(star => { 
+		starArry.forEach(star => {
 			star.classList.remove('reveal');
-			star.classList.add('spin'); 
+			star.classList.add('spin');
 		});
 
-		if(!dots[0].classList.contains('dot--gone')) {
+		if (!dots[0].classList.contains('dot--gone')) {
 			dots.forEach(dot => {
 				dot.classList.add('dot--gone');
 			});
 		}
 	}
+
 	let lastScroll = 0;
 	let direction = true;
 
@@ -34,63 +32,114 @@ window.onload = function event() {
 	window.addEventListener('scroll', scrolling, false);
 
 	function scrolling(e) {
-
 		const windowTop = window.pageYOffset;
 		const topbar = document.querySelector('.topbar');
 		const logo = document.querySelector('.logo');
 		const nav = document.querySelector('.nav');
 		const portfolio = document.querySelector('#portfolio-slice');
-	   	const PortfolioTop = portfolio.getBoundingClientRect().top;
+		const PortfolioTop = portfolio.getBoundingClientRect().top;
 		const stars = document.querySelectorAll('.fa-star');
-		const tower = document.querySelector('.tower');
-		const signals = document.querySelectorAll('.signal');
 		const socials = document.querySelectorAll('.social--media');
 		const cart = document.querySelector('.cart');
 		const gift = document.querySelector('.fact__gift');
-		
-		if(windowTop > 40 && !topbar.classList.contains('scroll')) {
-			logo.classList.add('logo--scroll') ;
+
+		if (windowTop > 40 && !topbar.classList.contains('scroll')) {
+			logo.classList.add('logo--scroll');
 			topbar.classList.add('topbar--scroll');
 			nav.classList.add('nav--scroll');
 		}
-		if(windowTop < 40 || windowTop < lastScroll) {
-			if(logo.classList.contains('logo--scroll')) logo.classList.remove('logo--scroll');
-			if(topbar.classList.contains('topbar--scroll')) topbar.classList.remove('topbar--scroll');
-			if(nav.classList.contains('nav--scroll')) nav.classList.remove('nav--scroll');
+		if (windowTop < 40 || windowTop < lastScroll) {
+			if (logo.classList.contains('logo--scroll'))
+				logo.classList.remove('logo--scroll');
+			if (topbar.classList.contains('topbar--scroll'))
+				topbar.classList.remove('topbar--scroll');
+			if (nav.classList.contains('nav--scroll'))
+				nav.classList.remove('nav--scroll');
 		}
-		
+
 		if (PortfolioTop <= -30) {
+			if (cart.classList.contains('none'))
+				setTimeout(function() {
+					cartSwap();
+				}, 1000);
 
-			if(cart.classList.contains('none')) setTimeout(function(){ cartSwap(); }, 1000); 
+			if (cart.classList.contains('none')) cart.classList.remove('none');
 
-			if(cart.classList.contains('none')) cart.classList.remove('none');
-
-			if(gift.classList.contains('none')) gift.classList.remove('none');
-
-			if(tower.classList.contains('none')) tower.classList.remove('none');
-
-			if(!signals[0].classList.contains('blinking')) {
-				signals.forEach(signal => { 
-					signal.classList.add('blinking');
-				});
-			}
+			if (gift.classList.contains('none')) gift.classList.remove('none');
 
 			if (!stars[0].classList.contains('in')) {
-				stars.forEach(star => { 
+				stars.forEach(star => {
 					star.classList.add('reveal');
-					star.classList.add('in'); 
+					star.classList.add('in');
 				});
 			}
 
 			if (socials[0].classList.contains('none')) {
-				socials.forEach(social => { 
+				socials.forEach(social => {
 					social.classList.remove('none');
 				});
 			}
 
-			if(stars[0].classList.contains('reveal')) setTimeout(function(){ swap(); }, 2000); 
+			if (stars[0].classList.contains('reveal'))
+				setTimeout(function() {
+					swap();
+				}, 2000);
 		}
 
 		lastScroll = windowTop;
 	}
-}
+};
+
+//------------------------
+// Contact Form
+//------------------------
+
+$('#submit').click(function(e) {
+	let fname = $('#name').val();
+	let phone = $('#phone').val();
+	let email = $('#email').val();
+	let inquiry = $('#inquiry').val();
+	let website = $('#website').val();
+	let budget = $('#budget').val();
+	var websiteCheck = document.getElementById('website--check').checked;
+	var seoCheck = document.getElementById('seo--check').checked;
+	var hostingCheck = document.getElementById('hosting--check').checked;
+
+	// let websiteCheck = (websiteIsChecked) ? 'true' : 'false';
+	// let seoCheck = ($(seoIsChecked).is(':clicked')) ? 'true' : 'false';
+	// let hostingCheck = (hostingIsChecked) ? 'true' : 'false';
+	$('#returnmessage').empty(); // To empty previous error/success message.
+	// console.log($('#website--check').is(':clicked'));
+	// Checking for blank fields
+	if (fname == '' || email == '' || inquiry == '') {
+		alert('Please Fill Required Fields');
+		e.preventDefault();
+	} else {
+		// Returns successful data submission message when the entered information is stored in database.
+		$.post(
+			'../php/contact_form1.php',
+			{
+				fname1: fname,
+				phone1: phone,
+				email1: email,
+				inquiry1: inquiry,
+				website1: website,
+				budget1: budget,
+				websiteCheck1: websiteCheck,
+				seoCheck1: seoCheck,
+				hostingCheck1: hostingCheck
+			},
+			function(data) {
+				$('#returnmessage').append(data); // Append returned message to message paragraph.
+
+				if (
+					data ==
+					'Your Query has been received, We will contact you soon.'
+				) {
+					$('#inquiryform')[0].reset(); // To reset form fields on success.
+				}
+			}
+		);
+		e.preventDefault();
+	}
+});
---
+++ php/contact_form1.php
@@ -0,0 +1,50 @@
+<?php
+
+/* Set e-mail recipient */
+$myemail = "estrauss2018@gmail.com";
+$subject = "Portfolio Contact Message";
+
+// Fetching Values from URL.
+$fname = $_POST['fname1'];
+$phone = $_POST['phone1'];
+$email = $_POST['email1'];
+$inquiry = $_POST['inquiry1'];
+$website = $_POST['website1'];
+$budget = $_POST['budget1'];
+$websiteCheck = $_POST['websiteCheck1'];
+$seoCheck = $_POST['seoCheck1'];
+$hostingCheck = $_POST['hostingCheck1'];
+
+$email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing E-mail.
+// After sanitization Validation is performed
+
+/* Validate required fields*/
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!preg_match("/^[a-zA-Z ]*$/",$fname)) {
+    echo "<span>* Please Fill Valid Contact No. *</span>";
+    } else {
+    /* Format form data for email */
+    $message = "
+    Name: $fname
+    E-mail: $email
+    Phone #: $phone
+    Previous Website: $website
+    budget: $budget
+    websiteCheck: $websiteCheck
+    seoCheck: $seoCheck
+    hostingCheck: $hostingCheck
+
+    Comments:
+    $inquiry
+
+    End of message";
+
+    /* Send the message using mail() function */
+    mail($myemail, $subject, $message);
+
+    echo "Your Query has been received, We will contact you soon.";
+    }
+    } else {
+echo "Invalid Email";
+}
+?>
